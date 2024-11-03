632fa0e1770182622e7ec8c831c059ed8c067e05
Khaeruladzkia <23176005@mhs.stmik-tegal.ac.id>
2024-11-03 16:02:12 +0700

    Add files via upload

---
+++ SI_RegPagi-23176005-Latihan18-Kelipatan.java
@@ -0,0 +1,19 @@
+/**
+ * Nama                 : Khaerul Adzkia Alamsyah
+ * Nim                  : 23176005
+ * Prodi                : Sistem Informasi / Semester 3
+ * Mata Kuliah          : PBO 1 / Pertemuan 6 / Latihan 18 - Kelipatan
+ * Deskripsi Program    : Mencetak kelipatan 3.5 dari 3.5 hingga 35 menggunakan perulangan for
+ * @author
+ **/
+package pertemuan.pkg6;
+public class Latihan18 {
+ 
+    public static void main(String[] args) {
+        for (double i = 3.5; i <= 35; i += 3.5) {
+            System.out.println(i);
+        }
+    }
+}
+
+
